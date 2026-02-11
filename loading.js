5e44569a81654de917931a9942d5731b3a2d520d
Fatii78 <fatii78jj@gmail.com>
2026-02-11 13:58:14 +0300

    add animate

--- loading.js
+++ loading.js
@@ -3,7 +3,7 @@ const percentEl = document.getElementById("percent")
 
 let p = 0
 const timer = setInterval(() => {
-  p += 1
+  p += 3
   fill.style.width = p + "%"
   percentEl.textContent = p + "%"
 
@@ -11,4 +11,4 @@ const timer = setInterval(() => {
     clearInterval(timer)
     window.location.href = "home.html"
   }
-}, 50) 
+}, 40) 
